3a5b86de9f4c63e9f5fc9a1009d474641517afa4
Merannix <Yupengh4@gmail.com>
2025-04-07 00:43:56 -0400

    Created Initial Apps
    Created apps: accounts, api, chatbot, home, packing, tips, trips, weather
    
    Added .gitignore for volatile and unneeded files.

--- TravelMate/settings.py
+++ TravelMate/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'trips',
+    'weather',
+    'tips',
+    'packing',
+    'chatbot',
+    'api',
+    'accounts',
+    'home',
 ]
 
 MIDDLEWARE = [
