2cbc8beafa423edec7aadf29bb3fdc1c93dc7974
Omar Kanteh <125144887+okanteh1-gsu@users.noreply.github.com>
2025-04-11 02:13:55 -0400

    Weather API Integration with Django
    Django project that fetches weather data by converting a city name to coordinates using Radar.io, then retrieves weather info from OpenWeatherMap

--- TravelMate/settings.py
+++ TravelMate/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +132,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# load .env
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+OWM_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+GEOCODE_API_KEY = os.getenv('GEOCODE_API_KEY')
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
 
 # Create your views here.
+@require_GET
+def get_weather_view(request):
+    query = request.GET.get('city')
+    if not query:
+        return JsonResponse({"error": "query parameter is required"}, status=400)
+    geo_url = f"https://api.radar.io/v1/geocode/forward?query={query}&limit=1"
+    headers = {
+        "Authorization" : settings.GEOCODE_API_KEY
+    }
+    response = requests.get(geo_url, headers=headers)
+    if response.status_code != 200:
+        return JsonResponse({"error": "failed to geocode address"}, status=response.status_code)
+    geocode_results = response.json()
+
+    if len(geocode_results.get("addresses", [])) == 0:
+        return JsonResponse({"error": "No addresses found"})
+    print(geocode_results["addresses"][0]['geometry']['coordinates'][1])
+    country = geocode_results['addresses'][0]['country']
+    city = geocode_results["addresses"][0].get('formattedAddress')
+    latitude =geocode_results["addresses"][0]['geometry']['coordinates'][1]
+    longitude = geocode_results["addresses"][0]['geometry']['coordinates'][0]
+
+    weather_url = (
+        f"https://api.openweathermap.org/data/3.0/onecall?"
+        f"lat={latitude}&lon={longitude}&"
+        f"exclude=minutely,hourly&"
+        f"units=metric&"
+        f"appid={settings.OWM_API_KEY}"
+    )
+    weather_response = requests.get(weather_url)
+    if weather_response.status_code != 200:
+        return JsonResponse({"error": "could not fetch weather details."}, status=weather_response.status_code)
+    weather_forecast = weather_response.json()
+    result = {
+        "country": country,
+        "city": city,
+        "latitude": latitude,
+        "longitude": longitude,
+        "weather_forecast": weather_forecast
+    }
+    return JsonResponse(result)
+
+
+# http://localhost:8000/weather/api/weather/?city=china
