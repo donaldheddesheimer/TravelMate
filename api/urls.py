cd7fd094a4caf880ba071a05f52442bed03bfe66
Merannix <Yupengh4@gmail.com>
2025-04-13 23:57:32 -0400

    Updated Google API

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('weather/', views.weather_forecast, name='weather_api'),
     path('places/', views.place_search, name='places_api'),
+    path('places/autocomplete/', views.place_autocomplete, name='places_autocomplete'),
+    path('places/<str:place_id>/', views.place_details, name='place_details'),
     path('chat/', views.chatbot, name='chatbot_api'),
 ]
