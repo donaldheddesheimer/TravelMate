8a711d981ce603f9aa9393162f2c28355da27ff0
Merannix <Yupengh4@gmail.com>
2025-04-21 17:24:24 -0400

    Fixed trip editing and delete

--- trips/views.py
+++ trips/views.py
@@ -1,4 +1,3 @@
-# trips/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Trip
@@ -16,15 +15,12 @@ def trip_create(request):
         if form.is_valid():
             trip = form.save(commit=False)
             trip.user = request.user
+
+            activities_list = request.POST.getlist('activities')
+            valid_activities = [act.strip() for act in activities_list if act.strip()]
+            trip.activities = "\n".join(valid_activities)
+
             trip.save()
-            # Process activities
-            activities = request.POST.getlist('activities')
-            if activities:
-                # Filter out empty activities
-                valid_activities = [act.strip() for act in activities if act.strip()]
-                if valid_activities:
-                    trip.activities = "\n".join(valid_activities)
-                    trip.save()
             return redirect('trips:list')
     else:
         form = TripForm()
@@ -42,14 +38,37 @@ def trip_dashboard(request, trip_id):
 @login_required
 def trip_edit(request, trip_id):
     trip = get_object_or_404(Trip, id=trip_id, user=request.user)
+    existing_activities = []
+    if trip.activities:
+        existing_activities = [act for act in trip.activities.split('\n') if act.strip()]
+
     if request.method == 'POST':
         form = TripForm(request.POST, instance=trip)
         if form.is_valid():
-            form.save()
+            trip_instance = form.save(commit=False)
+
+            activities_list = request.POST.getlist('activities')
+            valid_activities = [act.strip() for act in activities_list if act.strip()]
+            trip_instance.activities = "\n".join(valid_activities)
+
+            trip_instance.save()
             return redirect('trips:dashboard', trip_id=trip.id)
-    else:
+        else:
+            # If form is invalid, re-render with errors and original activities
+            # (The 'existing_activities' list defined earlier is used here)
+             pass # Fall through to render below
+
+    else: # GET request
         form = TripForm(instance=trip)
-    return render(request, 'trips/trip_edit.html', {'form': form, 'trip': trip})
+
+    # Prepare context for both GET and invalid POST rendering
+    context = {
+        'form': form,
+        'trip': trip,
+        'existing_activities': existing_activities
+    }
+    return render(request, 'trips/trip_edit.html', context)
+
 
 @login_required
 def trip_delete(request, trip_id):
@@ -57,4 +76,5 @@ def trip_delete(request, trip_id):
     if request.method == 'POST':
         trip.delete()
         return redirect('trips:list')
-    return render(request, 'trips/trip_list.html', {'trip': trip})
+    else:
+        return redirect('trips:list')
